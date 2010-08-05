f5b8ef699f360eaa853294dae79e72666d76e326
Indrajit Raychaudhuri <irc@indrajit.com>
2010-08-05 23:41:52 +0530

    add SBT support in archetypes

---
+++ lift-archetype-blank/src/main/resources/archetype-resources/project/build/LiftProject.scala
@@ -0,0 +1,13 @@
+import sbt._
+
+class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
+
+  // Add Maven Local repository for SBT to search for (disable if this doesn't suit you)
+  val mavenLocal = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
+
+#if ($liftVersion.endsWith("SNAPSHOT"))
+  // Add snapshot repo, since Lift SNAPSHOT in use
+  val snapshots = ScalaToolsSnapshots
+#end
+  
+}
